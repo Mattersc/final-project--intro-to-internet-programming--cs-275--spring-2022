0515a78fac8a922171e5d998f7efc6ab1a3d0b68
Mattersc <98325497+Mattersc@users.noreply.github.com>
2022-05-10 11:54:20 -0400

    Update gulpfile.js
    added pipeline for html compress/validation

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
-const { src, dest, series, watch } = require(`gulp`);
+const { src, dest, series, watch } = require(`gulp`),
+htmlValidator = require('gulp-html'),
+htmlCompressor = require('gulp-htmlmin');
+
+let htmlValidate = () => {
+    return src('*.html').pipe(htmlValidator())
+    .pipe(gulp.dest('valid'));
+}
+
+let htmlCompress = () => {
+    return src('*.html')
+    .pipe(htmlCompressor())
+    .pipe(dest('prod'));
+}
