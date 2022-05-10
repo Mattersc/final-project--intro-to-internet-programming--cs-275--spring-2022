b6217cfe884db7ab5865e4d8fafc7cba1e517856
Mattersc <98325497+Mattersc@users.noreply.github.com>
2022-05-10 12:41:13 -0400

    Update gulpfile.js
    added serve task, edited folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const { src, dest, series, watch } = require(`gulp`),
+browserSync = require('browser-sync'),
 htmlValidator = require('gulp-html'),
 htmlCompressor = require('gulp-htmlmin'),
 cssCleaner = require ('gulp-clean-css'),
 jsCompressor = require('gulp-uglify'),
 babel = require('gulp-babel'),
-gulpStyleLint = require('gulp-stylelint');
-
+gulpStyleLint = require('gulp-stylelint'),
+gulpESLint = require('gulp-eslint'),
+refresh = browserSync.reload;
 
 let htmlValidate = () => {
     return src('*.html').pipe(htmlValidator())
@@ -28,17 +30,41 @@ let transpileDev = () => {
     return src('js/*.js')
     .pipe(babel())
     .pipe(jsCompressor())
-    .pipe(gulp.dest('transpiles'));
+    .pipe(gulp.dest('new/js'));
 }
 
 let transpileProd = () => {
-    return src('img/*')
+    return src('js/*.js')
+    .pipe(babel())
+    .pipe(jsCompressor())
+    .pipe(gulp.dest('prod/js'));
+}
+
+let transpileImgProd = () => {
+    return src('img/*.jpg')
     .pipe(gulp.dest('prod/img'));
 }
 
 let cssLinter = () => {
     return src('css/style.css')
-    .pipe(gulpStylelint({
-        reporters: [{formatter: 'string', console: true}]}))
+    .pipe(gulpStylelint({reporters:
+        [{formatter: 'string', console: true}]}))
     .pipe(gulp.dest('lcss'));
 }
+
+let serve = () => {
+    browserSync({
+        browser: 'default',
+        server: {baseDir: `new`}
+    });
+
+    gulp.watch('js/*.js', series(gulpESLint, transpileDev))
+        gulp.on('change', refresh);
+
+    gulp.watch('css/*.css', cssLinter)
+        gulp.on('change', refresh);
+
+    gulp.watch('*.html', validateHTML)
+        gulp.on('change', refresh);
+};
+
